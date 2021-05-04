0feb8f109ad5e026f05ad68aff48cd99ec4be67e
Adam Raine <ad.st.raine@gmail.com>
2021-05-04 15:12:25 -0400

    add to fr config

--- types/artifacts.d.ts
+++ types/artifacts.d.ts
@@ -28,7 +28,6 @@ declare global {
       | 'ImageElements'
       | 'InspectorIssues'
       | 'JsUsage'
-      | 'LinkElements'
       | 'MainDocumentContent'
       | 'Manifest'
       | 'MixedContent'
